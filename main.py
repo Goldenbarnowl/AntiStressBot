6606592bbbc42279083cb84b38b3cd2a37aac22b
Nail <playday999@mail.ru>
2025-12-01 23:38:11 +0600

    v0.3 course update

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from aiogram.methods import DeleteWebhook
 
 from config import dp, bot
 from src.courses_directory.course_one import course_one_router
+from src.courses_directory.course_two import course_two_router
 from src.router import user_router
 
 
 async def start():
     dp.include_router(user_router)
-    user_router.include_router(course_one_router)
+    user_router.include_routers(course_one_router, course_two_router)
     try:
         await bot(DeleteWebhook(drop_pending_updates=True))
         await dp.start_polling(bot)
--- src/keyboard.py
+++ src/keyboard.py
@@ -4,7 +4,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 menu_slovar = {"buttonkey1": "Тестирование", "buttonkey2": "Курсы", "buttonkey3": "FAQ",}
 faqs = {"faq1": "Вопрос 1", "faq2": "Вопрос 2", "faq3": "Вопрос 3", "faq4": "Вопрос 4"}
 faq_answers = {"faq1": "Ответ 1", "faq2": "Ответ 2", "faq3": "Ответ 3", "faq4": "Ответ 4"}
-courses_slovar = {"course1": "Курс 1", "course2": "Курс 2", "course3": "Курс 3", "course4": "Курс 4"}
+courses_slovar = {"course1": "Курс 1", "course2": "Курс 2"}
 
 def menu_keyboard_maker():
     builder = ReplyKeyboardBuilder()
